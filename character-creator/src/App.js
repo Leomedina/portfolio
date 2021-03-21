5e0ab3dbc40e53aa58822b103370b9b012b52249
Leo Medina <medinayleo@gmail.com>
2021-03-21 19:01:22 -0400

    added styled bar to the background

--- character-creator/src/App.js
+++ character-creator/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import CharacterEditor from './components/CharacterEditor';
 import Footer from './components/Footer';
+import BackgroundBar from './components/BackgroundBar';
 
 function App() {
   return (
     <>
+      <BackgroundBar />
       <CharacterEditor />
       <Footer />
     </>
---
+++ character-creator/src/components/BackgroundBar/BackgroundBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './BackgroundBar.module.css';
+
+
+function BackgroundBar() {
+    return (
+        <div className={styles.backgroundBar}></div>
+    );
+};
+
+export default BackgroundBar;
